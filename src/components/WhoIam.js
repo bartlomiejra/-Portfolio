d394cd60f4f40021937d303f4e9aff7e00af7cf3
bartlomiejra <bartlomiej.ra@gmail.com>
2021-06-28 18:05:51 +0200

    ASD

--- src/components/WhoIam.js
+++ src/components/WhoIam.js
@@ -5,19 +5,19 @@ import './WhoIam.css'
 
 function WhoIam() {
 	return (
-		<div className="footer-container">
-			<section className="footer-subscription">
+		<div className="whoiam-container">
+			<section className="whoiam-subscription">
 
-				<p className="footer-subscription-heading">
+				<p className="whoiam-subscription-heading">
 					About Me
 				</p>
 	
-				{/* <p className="footer-subscription-text">
+				{/* <p className="whoiam-subscription-text">
 					You can unsubscribe at any time
 				</p>
 				<div className="input-areas">
 					<form >
-<input type="emial" name="emial" placeholder="Your Email" className="footer-input" />
+<input type="emial" name="emial" placeholder="Your Email" className="whoiam-input" />
 <Button buttonStyle="btn--outline"> Subscribe	
 
 </Button>
@@ -29,9 +29,9 @@ function WhoIam() {
 
 
 
-			 <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          {/* <div className='footer-link-items'> */}
+			 <div className='whoiam-links'>
+        <div className='whoiam-link-wrapper'>
+          {/* <div className='whoiam-link-items'> */}
             {/* <h2>About Us</h2> */}
             {/* <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
@@ -39,7 +39,7 @@ function WhoIam() {
             <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link> */}
           {/* </div> */}
-          {/* <div className='footer-link-items'> */}
+          {/* <div className='whoiam-link-items'> */}
             {/* <h2>Contact Us</h2> */}
             {/* <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
@@ -47,15 +47,15 @@ function WhoIam() {
             <Link to='/'>Sponsorships</Link> */}
           {/* </div> */}
         </div>
-        <div className='footer-link-wrapper'>
-          {/* <div class='footer-link-items'> */}
+        <div className='whoiam-link-wrapper'>
+          {/* <div class='whoiam-link-items'> */}
             {/* <h2>Videos</h2> */}
             {/* <Link to='/'>Submit Video</Link>
             <Link to='/'>Ambassadors</Link>
             <Link to='/'>Agency</Link>
             <Link to='/'>Influencer</Link> */}
           {/* </div> */}
-          {/* <div className='footer-link-items'> */}
+          {/* <div className='whoiam-link-items'> */}
             {/* <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
