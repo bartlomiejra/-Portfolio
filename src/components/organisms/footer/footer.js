8c419f5ff900e2be8a3d9ca01f24e62323b23403
bartlomiejra <bartlomiej.ra@gmail.com>
2022-12-14 22:18:47 +0100

    up

--- src/components/organisms/footer/footer.js
+++ src/components/organisms/footer/footer.js
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
-import Sound from 'react-sound';
+// import Sound from 'react-sound';
 import TheOtherSide from './music.mp3';
 import './footer.css';
 
 // import {Frame,   Scroll } from "framer"
 // super(props);
 const PlaySound = (
-    handleSongLoading,
-    handleSongPlaying,
-    handleSongFinishedPlaying,
+  handleSongLoading,
+  handleSongPlaying,
+  handleSongFinishedPlaying,
 ) => {
-    const [isPlaying, setIsPlaying] = useState(false);
-    return (
-        <footer>
-            <button onClick={() => setIsPlaying(!isPlaying)}>
-                {!isPlaying ? '@' : <i class="fas fa-volume-up"> </i>}
-            </button>
-            {new Date().getFullYear()} by bartlomiejra
-            <Sound
-                url={TheOtherSide}
-                playStatus={
-                    isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED
-                }
-                playFromPosition={300}
-                onLoading={handleSongLoading}
-                onPlaying={handleSongPlaying}
-                onFinishedPlaying={handleSongFinishedPlaying}
-            />
-        </footer>
-    );
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    //     <button onClick={() => setIsPlaying(!isPlaying)}>
+    //         {!isPlaying ? '@' : <i class="fas fa-volume-up"> </i>}
+    //     </button>
+    //     <Sound
+    //         url={TheOtherSide}
+    //         playStatus={
+    //             isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED
+    //         }
+    //         playFromPosition={300}
+    //         onLoading={handleSongLoading}
+    //         onPlaying={handleSongPlaying}
+    //         onFinishedPlaying={handleSongFinishedPlaying}
+    //     />
+    <footer>
+      <>{new Date().getFullYear()} by bartlomiejra</>
+    </footer>
+  );
 };
 
 export default PlaySound;
